304220a9bf74188308e7b897a6ddf52d035b58d1
Leo Torres <leotorresval92@gmail.com>
2021-11-15 18:27:25 -0500

    Clase empleado añadida

---
+++ Empleado.java
@@ -0,0 +1,16 @@
+public class Empleado(){
+
+	private String cedula;
+	private String nombre;
+
+	public String getCedula(){
+		return this.cedula;
+	}
+
+	public String getNombre(){
+		return this.nombre
+	}
+
+
+}
+
